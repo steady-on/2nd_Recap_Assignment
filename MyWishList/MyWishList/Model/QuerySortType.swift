a4fd18e58766f0d49dee53e9eef8e05e67d85f5a
steady-on <roen.steady.on@gmail.com>
2023-09-08 00:53:44 +0900

    add: QuerySortType 열거형 정의
    - api query를 요청할 때 sort 파라미터에 들어갈 값을 열거형으로 정의
    - 나중에 View에서 버튼으로 만들기 위해 labelText를 정의

---
+++ MyWishList/MyWishList/Model/QuerySortType.swift
@@ -0,0 +1,33 @@
+//
+//  QuerySortType.swift
+//  MyWishList
+//
+//  Created by Roen White on 2023/09/08.
+//
+
+import Foundation
+
+enum QuerySortType: CaseIterable {
+    case accuracy
+    case date
+    case ascending
+    case descending
+    
+    var value: String {
+        switch self {
+        case .accuracy: return "sim"
+        case .date: return "date"
+        case .ascending: return "asc"
+        case .descending: return "dsc"
+        }
+    }
+    
+    var labelText: String {
+        switch self {
+        case .accuracy: return "정확도순"
+        case .date: return "최신순"
+        case .ascending: return "오름차순"
+        case .descending: return "내림차순"
+        }
+    }
+}
