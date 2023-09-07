0c58248db5e41fd6daea5d316dd366d9e3797ef4
steady-on <roen.steady.on@gmail.com>
2023-09-07 20:06:44 +0900

    init: create Project file and basic settings

---
+++ MyWishList/MyWishList/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  MyWishList
+//
+//  Created by Roen White on 2023/09/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
