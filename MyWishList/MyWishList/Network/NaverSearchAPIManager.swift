06dbc2cd97fdfd5017c90983b0a188033fdb6c45
steady-on <roen.steady.on@gmail.com>
2023-09-08 14:56:44 +0900

    add: NaverSearchAPIManager
    -Naver API를 통해 검색을 수행하고 data를 codable 구조체로 decoding할 NaverSearchAPIManager 정의
    - 효율적인 메모리 관리 및 클래스 내부 프로퍼티 공유를 위해 싱글턴 패턴을 적용
    - func search(for:sortedBy:nextPage:completionHandeler:): 검색키워드나 정렬방식, 다음페이지 fetching 여부를 매개변수로  전달 받아 url의 파라미터를 설정
    - func configureParameters(keyword: sortType:nextPage): 검색키워드, 정렬타입, 페이지에 관련한 변수를 관리하고 결과를 URLQueryItem배열로 만들어서 반환
    - performRequest(for:completionHandler): URLComponents를 매개변수로 받아 HTTPHeader를 추가하여 request를 수행
    -  func parseJSON(_:) → T?: 매개변수로 받은 데이터를 제네릭으로 전달받은 타입으로 디코딩하여 반환
    - performRequest와 parseJSON 메서드는 제네릭을 적용해서 다른 타입이나 naver API의 다른 요청에도 재사용 가능하도록 함

---
+++ MyWishList/MyWishList/Network/NaverSearchAPIManager.swift
@@ -0,0 +1,121 @@
+//
+//  NaverSearchAPIManager.swift
+//  MyWishList
+//
+//  Created by Roen White on 2023/09/07.
+//
+
+import Foundation
+
+final class NaverSearchAPIManager {
+    static let shared = NaverSearchAPIManager()
+    
+    private init() {}
+    
+    private let baseURL = "https://openapi.naver.com/v1/search/shop.json"
+    private let headers = [
+        "X-Naver-Client-Id" : APIKey.naverClientID,
+        "X-Naver-Client-Secret" : APIKey.naverClientSecret
+    ]
+    private let displayItemCount = 30
+    
+    private var keyword: String = ""
+    private var sortType: QuerySortType = .accuracy
+    private var start = 1
+    private var total = 1000
+    
+    func search(for keyword: String? = nil, sortedBy sortType: QuerySortType? = nil, nextPage: Bool = false, completionHandler: @escaping (Result<[Item], NetworkError>) -> ()) {
+        guard var urlComponents = URLComponents(string: baseURL) else {
+            completionHandler(.failure(.invalidRequestError))
+            return
+        }
+        
+        urlComponents.queryItems = configureParameters(keyword: keyword, sortType: sortType, nextPage: nextPage)
+        
+        performRequest(for: urlComponents) { (result: Result<QueryResult, NetworkError>) in
+            switch result {
+            case .success(let queryResult):
+                self.total = queryResult.total <= 1000 ? queryResult.total : 1000
+                
+                DispatchQueue.main.async {
+                    completionHandler(.success(queryResult.items))
+                }
+                
+            case .failure(let error):
+                completionHandler(.failure(error))
+            }
+        }
+    }
+    
+    private func configureParameters(keyword: String?, sortType: QuerySortType?, nextPage: Bool) -> [URLQueryItem] {
+        if let keyword, keyword != self.keyword {
+            self.keyword = keyword
+            self.sortType = .accuracy
+            self.start = 1
+        }
+        
+        if let sortType {
+            self.sortType = sortType
+            self.start = 1
+        }
+        
+        if nextPage {
+            start = start + displayItemCount <= total ? start + displayItemCount : displayItemCount
+        }
+        
+        return [.init(keyword: self.keyword), .init(sortType: self.sortType), .init(displayItemCount: self.displayItemCount), .init(startValue: start)]
+    }
+    
+    private func performRequest<T: Codable>(for urlComponents: URLComponents, completionHandler: @escaping (Result<T, NetworkError>) -> ()) {
+        guard let url = urlComponents.url else {
+            completionHandler(.failure(.invalidRequestError))
+            return
+        }
+        
+        let session = URLSession.shared
+        var request = URLRequest(url: url, timeoutInterval: 10.0)
+        request.allHTTPHeaderFields = headers
+        
+        let task = session.dataTask(with: request) { data, response, error in
+            guard error == nil else {
+                completionHandler(.failure(.notAccessNetwork))
+                return
+            }
+            
+            guard let response = response as? HTTPURLResponse else {
+                completionHandler(.failure(.notAccessNetwork))
+                return
+            }
+            
+            guard response.statusCode == 200, let data else {
+                if response.statusCode == 500 {
+                    completionHandler(.failure(.serverError))
+                    return
+                }
+                
+                completionHandler(.failure(.invalidRequestError))
+                return
+            }
+            
+            guard let decodedData: T = self.parseJSON(data) else {
+                completionHandler(.failure(.jsonParseError))
+                return
+            }
+            
+            completionHandler(.success(decodedData))
+        }
+        
+        task.resume()
+    }
+    
+    private func parseJSON<T: Codable> (_ jsonData: Data) -> T? {
+        let decoder = JSONDecoder()
+        
+        do {
+            let decodedData = try decoder.decode(T.self, from: jsonData)
+            return decodedData
+        } catch {
+            return nil
+        }
+    }
+}
