a036ec4eb927fe1b2b2548350155490b57e6ffdf
steady-on <roen.steady.on@gmail.com>
2023-09-08 18:24:01 +0900

    add: BaseCollectionView

---
+++ MyWishList/MyWishList/Base/BaseCollectionView.swift
@@ -0,0 +1,24 @@
+//
+//  BaseCollectionView.swift
+//  MyWishList
+//
+//  Created by Roen White on 2023/09/08.
+//
+
+import UIKit
+
+class BaseCollectionView: UICollectionView {
+
+    override init(frame: CGRect, collectionViewLayout layout: UICollectionViewLayout) {
+        super.init(frame: .zero, collectionViewLayout: layout)
+        
+        configure()
+    }
+    
+    @available(*, unavailable)
+    required init?(coder: NSCoder) {
+        fatalError("init(coder:) has not been implemented")
+    }
+    
+    func configure() {}
+}
