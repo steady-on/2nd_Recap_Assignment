b390d9dd5dd80421e585648cd0030a606c9f4460
steady-on <roen.steady.on@gmail.com>
2023-09-10 14:21:14 +0900

    add: Realm에서 사용할 모델 WishList 정의
    - Network Model인 Item을 받아와 초기화하는 생성자 추가

---
+++ MyWishList/MyWishList/Realm/WishList.swift
@@ -0,0 +1,46 @@
+//
+//  WishList.swift
+//  MyWishList
+//
+//  Created by Roen White on 2023/09/10.
+//
+
+import Foundation
+import RealmSwift
+
+final class WishList: Object {
+    @Persisted(primaryKey: true) var _id: ObjectId
+    @Persisted(indexed: true) var productID: String
+    @Persisted var title: String
+    @Persisted var mallName: String
+    @Persisted var link: String
+    @Persisted var priceInt: Int
+    @Persisted var priceString: String
+    @Persisted var image: Data
+    @Persisted var addedAt: Date
+    
+    convenience init(productID: String, title: String, mallName: String, link: String, priceInt: Int, priceString: String, image: Data) {
+        self.init()
+
+        self.productID = productID
+        self.title = title
+        self.mallName = mallName
+        self.link = link
+        self.priceInt = priceInt
+        self.priceString = priceString
+        self.image = image
+        self.addedAt = Date()
+    }
+    
+    convenience init(from item: Item, imageData: Data) {
+        self.init(
+            productID: item.productID,
+            title: item.title,
+            mallName: item.mallName,
+            link: item.link,
+            priceInt: item.priceInt,
+            priceString: item.priceString,
+            image: imageData
+        )
+    }
+}
