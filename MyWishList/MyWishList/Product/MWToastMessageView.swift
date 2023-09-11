d36c3875eb7a860aaa9d48ffdd69e1224ca0794a
steady-on <roen.steady.on@gmail.com>
2023-09-11 21:47:23 +0900

    feat: ProdectDetailWebView에서 찜버튼을 누를때 토스트 메세지 구현
    - collectionView에서는 바로 아이템이 사라지거나 view자체가 오밀조밀 아이템이 많아서 사용자의 UX를 좋지않게 만들것 같아 구현하지 않음

---
+++ MyWishList/MyWishList/Product/MWToastMessageView.swift
@@ -0,0 +1,90 @@
+//
+//  MWToastMessageView.swift
+//  MyWishList
+//
+//  Created by Roen White on 2023/09/11.
+//
+
+import UIKit
+
+class MWToastMessageView: BaseView {
+    
+    private lazy var messageLabel: UILabel = {
+        let label = UILabel()
+        label.font = .preferredFont(forTextStyle: .callout).bold()
+        label.textAlignment = .center
+        label.numberOfLines = 0
+        return label
+    }()
+    
+    private lazy var symbolImageView: UIImageView = {
+        let imageView = UIImageView()
+        imageView.contentMode = .scaleAspectFit
+        return imageView
+    }()
+    
+    init(toastType: ToastType) {
+        super.init()
+        
+        messageLabel.text = toastType.message
+        symbolImageView.image = UIImage(systemName: toastType.imageName)
+        symbolImageView.tintColor = toastType.imageColor
+    }
+    
+    override func configure() {
+        translatesAutoresizingMaskIntoConstraints = false
+        backgroundColor = .black.withAlphaComponent(0.4)
+        layer.cornerRadius = 15
+        clipsToBounds = true
+        
+        let components = [messageLabel, symbolImageView]
+        
+        components.forEach {
+            $0.translatesAutoresizingMaskIntoConstraints = false
+            addSubview($0)
+        }
+    }
+    
+    override func setConstraints() {
+        NSLayoutConstraint.activate([
+            symbolImageView.heightAnchor.constraint(equalTo: symbolImageView.widthAnchor, multiplier: 1),
+            symbolImageView.widthAnchor.constraint(equalTo: safeAreaLayoutGuide.widthAnchor, multiplier: 0.3),
+            symbolImageView.topAnchor.constraint(equalTo: layoutMarginsGuide.topAnchor, constant: 8),
+            symbolImageView.centerXAnchor.constraint(equalTo: centerXAnchor)
+        ])
+        
+        NSLayoutConstraint.activate([
+            messageLabel.centerXAnchor.constraint(equalTo: centerXAnchor),
+            messageLabel.topAnchor.constraint(equalTo: symbolImageView.bottomAnchor, constant: 4),
+            messageLabel.leadingAnchor.constraint(greaterThanOrEqualTo: layoutMarginsGuide.leadingAnchor),
+            messageLabel.trailingAnchor.constraint(lessThanOrEqualTo: layoutMarginsGuide.trailingAnchor),
+            messageLabel.bottomAnchor.constraint(equalTo: layoutMarginsGuide.bottomAnchor)
+        ])
+    }
+    
+    enum ToastType {
+        case save
+        case delete
+        
+        var message: String {
+            switch self {
+            case .save: return "위시리스트에 저장되었습니다."
+            case .delete: return "위시리스트에서 삭제되었습니다."
+            }
+        }
+        
+        var imageName: String {
+            switch self {
+            case .save: return "heart.fill"
+            case .delete: return "heart.slash"
+            }
+        }
+        
+        var imageColor: UIColor? {
+            switch self {
+            case .save: return .systemPink
+            case .delete: return .white
+            }
+        }
+    }
+}
--- MyWishList/MyWishList/Scene/ProductDetailWebViewController.swift
+++ MyWishList/MyWishList/Scene/ProductDetailWebViewController.swift
@@ -61,6 +61,12 @@ class ProductDetailWebViewController: BaseViewController {
     @objc private func toggleWishButtonTapped() {
         isInWishList.toggle()
         navigationItem.rightBarButtonItem?.image = wishButtonImage
+        
+        if isInWishList {
+            showToastMessage(style: .save)
+        } else {
+            showToastMessage(style: .delete)
+        }
     }
     
     override func setConstraints() {
@@ -70,5 +76,24 @@ class ProductDetailWebViewController: BaseViewController {
             webView.trailingAnchor.constraint(equalTo: view.safeAreaLayoutGuide.trailingAnchor),
             webView.bottomAnchor.constraint(equalTo: view.safeAreaLayoutGuide.bottomAnchor)
         ])
+    }
+    
+    private func showToastMessage(style: MWToastMessageView.ToastType) {
+        let toastView = MWToastMessageView(toastType: style)
+        
+        view.addSubview(toastView)
+        
+        NSLayoutConstraint.activate([
+            toastView.centerXAnchor.constraint(equalTo: view.centerXAnchor),
+            toastView.centerYAnchor.constraint(equalTo: view.centerYAnchor),
+            toastView.widthAnchor.constraint(equalTo: view.safeAreaLayoutGuide.widthAnchor, multiplier: 0.4),
+            toastView.heightAnchor.constraint(greaterThanOrEqualTo: toastView.widthAnchor, multiplier: 1)
+        ])
+        
+        UIView.animate(withDuration: 1.2, delay: 0.2, options: .curveLinear, animations: {
+            toastView.alpha = 0.0
+            }, completion: {(isCompleted) in
+                toastView.removeFromSuperview()
+            })
     }
 }
