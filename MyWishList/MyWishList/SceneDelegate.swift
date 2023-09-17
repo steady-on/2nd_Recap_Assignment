3d25476e55472a196457477f4c7404b1e0cfdd2a
steady-on <roen.steady.on@gmail.com>
2023-09-17 23:52:07 +0900

    feat: 앱이 실행될때 네트워크 상태를 모니터하고, 종료될때 모니터를 종료하도록 함
    - 약간의 오타 수정

--- MyWishList/MyWishList/SceneDelegate.swift
+++ MyWishList/MyWishList/SceneDelegate.swift
@@ -10,16 +10,18 @@ import UIKit
 class SceneDelegate: UIResponder, UIWindowSceneDelegate {
 
     var window: UIWindow?
-    var errorWindow: UIWindow?
     
     func scene(_ scene: UIScene, willConnectTo session: UISceneSession, options connectionOptions: UIScene.ConnectionOptions) {
         // Use this method to optionally configure and attach the UIWindow `window` to the provided UIWindowScene `scene`.
         // If using a storyboard, the `window` property will automatically be initialized and attached to the scene.
         // This delegate does not imply the connecting scene or session are new (see `application:configurationForConnectingSceneSession` instead).
+        
+        NetworkMonitor.shared.startMonitoring()
+        
         guard let scene = (scene as? UIWindowScene) else { return }
         window = UIWindow(windowScene: scene)
-        let rootViewcontroller = MWTabBarController()
-        window?.rootViewController = rootViewcontroller
+        let rootViewController = MWTabBarController()
+        window?.rootViewController = rootViewController
         window?.makeKeyAndVisible()
     }
 
@@ -28,6 +30,7 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // This occurs shortly after the scene enters the background, or when its session is discarded.
         // Release any resources associated with this scene that can be re-created the next time the scene connects.
         // The scene may re-connect later, as its session was not necessarily discarded (see `application:didDiscardSceneSessions` instead).
+        NetworkMonitor.shared.stopMonitoring()
     }
 
     func sceneDidBecomeActive(_ scene: UIScene) {
@@ -50,7 +53,5 @@ class SceneDelegate: UIResponder, UIWindowSceneDelegate {
         // Use this method to save data, release shared resources, and store enough scene-specific state information
         // to restore the scene back to its current state.
     }
-
-
 }
 
