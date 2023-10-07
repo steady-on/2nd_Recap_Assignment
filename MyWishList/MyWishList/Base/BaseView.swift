b90296a2d8d40e24bb69cce47da2dac01ca1f0e0
steady-on <roen.steady.on@gmail.com>
2023-10-07 23:14:24 +0900

    refactor: BaseView의 생성자 대신 draw 메서드로 로직 변경

--- MyWishList/MyWishList/Base/BaseView.swift
+++ MyWishList/MyWishList/Base/BaseView.swift
@@ -10,14 +10,19 @@ import UIKit
 class BaseView: UIView {
     init() {
         super.init(frame: .zero)
-        
-        configure()
-        setConstraints()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
+    }
+    
+    override func draw(_ rect: CGRect) {
+        super.draw(rect)
+        print(#function)
+        
+        configure()
+        setConstraints()
     }
     
     func configure() {}
