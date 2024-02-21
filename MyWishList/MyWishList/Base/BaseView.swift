5c5433ac9aaf1d45b64c7587dc998798dd5f7257
steady-on <roen.steady.on@gmail.com>
2024-02-21 15:03:08 +0900

    fix: BaseView의 컴포넌트들을 세팅하는 메서드를 init에서 호출하도록 수정

--- MyWishList/MyWishList/Base/BaseView.swift
+++ MyWishList/MyWishList/Base/BaseView.swift
@@ -10,18 +10,14 @@ import UIKit
 class BaseView: UIView {
     init() {
         super.init(frame: .zero)
+        
+        configure()
+        setConstraints()
     }
     
     @available(*, unavailable)
     required init?(coder: NSCoder) {
         fatalError("init(coder:) has not been implemented")
-    }
-    
-    override func draw(_ rect: CGRect) {
-        super.draw(rect)
-        
-        configure()
-        setConstraints()
     }
     
     func configure() {}
